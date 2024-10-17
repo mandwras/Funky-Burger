4374d354b486bab03bc35822f4753c07187e85bc
Giannis Mandoras <johnmand216@gmail.com>
2024-10-17 11:34:12 +0300

    shopping cart icon rdy

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-<header className="bg-gray-100 p-4 flex items-center justify-between shadow-neumorphic">
-  {/* Left Side: Logo */}
-  <img 
-    src="/icons/burger-logo.png" 
-    alt="Funky Burger Logo" 
-    className="w-16 h-16 object-contain" 
-  />
+  <header className="bg-gray-100 p-4 flex items-center justify-between shadow-neumorphic">
+    {/* Left Side: Logo */}
+    <img 
+      src="/icons/burger-logo.png" 
+      alt="Funky Burger Logo" 
+      className="w-16 h-16 object-contain" 
+    />
       
-      {/* Center: Navigation Links */}
-      <nav className="flex-grow flex justify-center space-x-3">
+          {/* Center: Navigation Links */}
+      <nav className="flex-grow flex justify-center space-x-3 items-center">
         <Link href="/" className="text-gray-900 font-semibold font-sans text-md">
           Home
         </Link>
@@ -21,14 +21,14 @@ const Header = () => {
           Menu
         </Link>
         <span className="text-gray-900 font-semibold">|</span> {/* Separator */}
-        <Link href="/" className="text-gray-900 font-semibold font-sans text-md cart-button">
-          Cart
-        </Link>
-        <span className="text-gray-900 font-semibold">|</span> {/* Separator */}
         <Link href="/" className="text-gray-900 font-semibold font-sans text-md">
           Info
         </Link>
-      </nav>
+        <span className="text-gray-900 font-semibold">|</span> {/* Separator */}
+        <Link href="/" className="text-gray-900 font-semibold font-sans text-md cart-button flex items-center">
+          <img src="/icons/cart.png" alt="Cart" className="w-9 h-9 object-contain" />
+        </Link>
+    </nav>
 
       {/* Right Side: Log In Button */}
       <Link href="login">
