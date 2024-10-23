1eeeeb46ac714bcd3ce62b5a1f43470295f5f569
Giannis Mandoras <johnmand216@gmail.com>
2024-10-23 14:00:35 +0300

    improved handleAddToCart console.log the id

--- src/app/components/menuItem.js
+++ src/app/components/menuItem.js
@@ -1,12 +1,11 @@
 "use client"
 import React, {useState, useEffect} from "react";
-import Image from "next/image";
-import { SUBRESOURCE_INTEGRITY_MANIFEST } from "next/dist/shared/lib/constants";
+
 
 const MenuItems = () => {
   //server
   const [items, setItems] = useState([])
-  const[loading, setLoading] = useState(true)
+  const[loading, setLoading] = useState(true) // not used yet
   //fetching
   useEffect(() => { 
     const fetchItems = async () => {
@@ -24,7 +23,8 @@ const MenuItems = () => {
     fetchItems();
   }, [])
 
-  const handleAddToCart = (e) => {
+  const handleAddToCart = (item, e) => {
+    console.log(item.id)
     const { pageX, pageY } = e; // Get cursor position 
     let cart;
     // if (window.scrollY < 100) {
@@ -81,7 +81,7 @@ const MenuItems = () => {
     <div className="p-8 flex-row" >
       <h2 className="text-center text-black text-3xl mb-6 pixel-font">Funky's Menu</h2>
       <div className="space-y-8 flex flex-col">
-        {items.map((items, index) => (
+        {items.map((item, index) => (
           <div
             key={index}
             className="flex flex-col md:flex-row items-center justify-between bg-gray-100 p-6 rounded-lg shadow-neumorphic"
@@ -89,26 +89,26 @@ const MenuItems = () => {
             {/* Left Side: Image and Text */}
             <div className="flex flex-col md:flex-row items-center">
               <img
-                src={items.image}
-                alt={items.name}
+                src={item.image}
+                alt={item.name}
                 className="w-32 h-32 object-cover rounded-lg mb-4 md:mb-0 md:mr-6"
               />
               <div>
-                <h3 className="pixel-font text-xl font-semibold text-gray-900">{items.name}<span className="p-1"></span>{items.price}$</h3>
-                <p className="pixel-font text-gray-700 mt-2">{items.description}</p>
+                <h3 className="pixel-font text-xl font-semibold text-gray-900">{item.name}<span className="p-1"></span>{item.price}$</h3>
+                <p className="pixel-font text-gray-700 mt-2">{item.description}</p>
                 <div className="border mt-4 w-[90px] shadow-neumorphic rounded-md border-gray-900 flex justify-center bg-emerald-300"> 
-                <button onClick={handleAddToCart} className="flex justify-center items-center text-purple-900 text-sm font-bold">Add to Cart</button>
+                <button onClick={(e) => handleAddToCart(item, e)} className="flex justify-center items-center text-purple-900 text-sm font-bold">Add to Cart</button>
                 </div>
               </div>
             </div>
             {/* Right Side: Icon and Text */}
             <div className="flex items-center space-x-2">
               <img
-                src={items.rightIcon}
-                alt={items.rightDescription}
+                src={item.rightIcon}
+                alt={item.rightDescription}
                 className="w-6 h-6 object-contain"
               />
-              <p className="pixel-font text-gray-700">{items.rightDescription}</p>
+              <p className="pixel-font text-gray-700">{item.rightDescription}</p>
             </div>
           </div>
         ))}
