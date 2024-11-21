192f7dab27c302a07d1fe7622bba980f7b3f5ac8
Giannis Mandoras <johnmand216@gmail.com>
2024-11-21 10:47:05 +0200

    style changes

--- src/app/check/page.js
+++ src/app/check/page.js
@@ -52,11 +52,11 @@ const Checkout = () => {
               {cart.map((item) => (
                 <li key={item.id} className="flex justify-between items-center">
                   <div>
+                    <div className="flex space-x-2">
                     <h2 className="text-lg font-semibold text-gray-600">
                       {item.name}
                     </h2>
-                    <p className="text-gray-300">{item.description}</p>
-                    <div className="flex items-center space-x-2">
+                    <div className="flex items-center space-x-1">
                       <button
                         onClick={() => handleQuantityChange(item, -1)}
                         className="px-2 text-red-500"
@@ -71,6 +71,9 @@ const Checkout = () => {
                         +
                       </button>
                     </div>
+                    </div>
+                    <p className="text-gray-300">{item.description}</p>
+
                   </div>
                   <span className="text-gray-900">
                     ${(item.price * item.quantity).toFixed(2)}
