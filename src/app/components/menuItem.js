170651fc62a097efa79c6e793070234b5385ab9a
Giannis Mandoras <johnmand216@gmail.com>
2024-11-27 10:00:10 +0200

    filter pos changed

--- src/app/components/menuItem.js
+++ src/app/components/menuItem.js
@@ -9,13 +9,13 @@ const MenuItems = ({ handleAddToCart }) => {
   const [filteredItems, setFilteredItems] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("all");
   const [loading, setLoading] = useState(true);
-  const [drawerOpen, setDrawerOpen] = useState(false); 
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   // Fetching items and categories from Supabase
   useEffect(() => {
     const fetchItemsAndCategories = async () => {
       try {
-        // products fetch
+        // Fetching products
         const { data: productsData, error: productsError } = await supabase
           .from("products")
           .select("*");
@@ -29,7 +29,7 @@ const MenuItems = ({ handleAddToCart }) => {
           console.log("Fetched items:", productsData);
         }
 
-        // katigories fetch
+        // Fetching categories
         const { data: categoriesData, error: categoriesError } = await supabase
           .from("categories")
           .select("*");
@@ -43,8 +43,8 @@ const MenuItems = ({ handleAddToCart }) => {
         }
       } catch (error) {
         console.log("Error fetching data from Supabase!", error);
-        setItems([]); 
-        setCategories([]); 
+        setItems([]);
+        setCategories([]);
       } finally {
         setLoading(false);
       }
@@ -53,16 +53,15 @@ const MenuItems = ({ handleAddToCart }) => {
     fetchItemsAndCategories();
   }, []);
 
-  // katigories k selection
+  // Category change handling
   const handleCategoryChange = (categoryId) => {
     setSelectedCategory(categoryId);
     if (categoryId === "all") {
-      setFilteredItems(items); 
+      setFilteredItems(items);
     } else {
       setFilteredItems(items.filter((item) => item.category_id === categoryId));
     }
   };
-
 
   const skeletonItems = Array(5).fill(0);
 
@@ -70,56 +69,11 @@ const MenuItems = ({ handleAddToCart }) => {
     <div className="p-8 flex-col relative">
 
       <div className="flex justify-between items-center mb-6">
-
-        <button
-          onClick={() => setDrawerOpen(!drawerOpen)}
-          className="px-6 py-3 bg-emerald-500 rounded-lg text-white font-semibold text-lg hover:bg-emerald-600 transition-all duration-200 sm:absolute sm:left-8 sm:top-8 flex items-center justify-center"
-        >
-
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" id="filter" className="w-4 h-4">
-            <path d="M4 10h7.09a6 6 0 0 0 11.82 0H44a1 1 0 0 0 0-2H22.91A6 6 0 0 0 11.09 8H4a1 1 0 0 0 0 2zM17 5a4 4 0 1 1-4 4A4 4 0 0 1 17 5zM44 23H36.91a6 6 0 0 0-11.82 0H4a1 1 0 0 0 0 2H25.09a6 6 0 0 0 11.82 0H44a1 1 0 0 0 0-2zM31 28a4 4 0 1 1 4-4A4 4 0 0 1 31 28zM44 38H22.91a6 6 0 0 0-11.82 0H4a1 1 0 0 0 0 2h7.09a6 6 0 0 0 11.82 0H44a1 1 0 0 0 0-2zM17 43a4 4 0 1 1 4-4A4 4 0 0 1 17 43z"></path>
-          </svg>
-        </button>
-
-
+        {/* Title */}
         <h2 className="text-center text-black text-3xl pixel-font flex-1">
           Funky&apos;s Menu
         </h2>
       </div>
-
-
-      {drawerOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 z-10 flex items-center justify-center">
-          <div className="bg-white p-6 rounded-lg w-3/4 sm:w-1/2 md:w-1/3 shadow-lg">
-            <h3 className="text-xl font-semibold text-center mb-4">Filter by Category</h3>
-            <div className="flex flex-col space-y-4">
-              <button
-                onClick={() => handleCategoryChange("all")}
-                className={`px-6 py-3 rounded-lg text-lg font-semibold ${selectedCategory === "all" ? "bg-emerald-500 text-white" : "bg-gray-200 text-black"} hover:bg-emerald-400 transition-all duration-200`}
-              >
-                All
-              </button>
-              {categories.map((category) => (
-                <button
-                  key={category.id}
-                  onClick={() => handleCategoryChange(category.id)}
-                  className={`px-6 py-3 rounded-lg text-lg font-semibold ${selectedCategory === category.id ? "bg-emerald-500 text-white" : "bg-gray-200 text-black"} hover:bg-emerald-400 transition-all duration-200`}
-                >
-                  {category.name}
-                </button>
-              ))}
-            </div>
-            <div className="mt-4 text-center">
-              <button
-                onClick={() => setDrawerOpen(false)}
-                className="px-6 py-3 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-all duration-200"
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
 
       {/* Menu Items */}
       <div className="space-y-8 flex flex-col">
@@ -180,7 +134,6 @@ const MenuItems = ({ handleAddToCart }) => {
                   </div>
                 </div>
 
-                {/* Item deksia */}
                 <div className="flex items-center space-x-2">
                   <Image
                     src={item.icon_url}
@@ -196,6 +149,52 @@ const MenuItems = ({ handleAddToCart }) => {
               </div>
             ))}
       </div>
+
+      {/* Filter */}
+      <button
+        onClick={() => setDrawerOpen(!drawerOpen)}
+        className="fixed bottom-8 left-1/2 transform -translate-x-1/2 px-6 py-3 bg-emerald-500 rounded-lg text-white font-semibold text-lg hover:bg-emerald-600 transition-all duration-200 flex items-center justify-center"
+      >
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" id="filter" className="w-4 h-4 mr-2">
+          <path d="M4 10h7.09a6 6 0 0 0 11.82 0H44a1 1 0 0 0 0-2H22.91A6 6 0 0 0 11.09 8H4a1 1 0 0 0 0 2zM17 5a4 4 0 1 1-4 4A4 4 0 0 1 17 5zM44 23H36.91a6 6 0 0 0-11.82 0H4a1 1 0 0 0 0 2H25.09a6 6 0 0 0 11.82 0H44a1 1 0 0 0 0-2zM31 28a4 4 0 1 1 4-4A4 4 0 0 1 31 28zM44 38H22.91a6 6 0 0 0-11.82 0H4a1 1 0 0 0 0 2h7.09a6 6 0 0 0 11.82 0H44a1 1 0 0 0 0-2zM17 43a4 4 0 1 1 4-4A4 4 0 0 1 17 43z"></path>
+        </svg>
+        Filter
+      </button>
+
+
+      {drawerOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-10 flex items-center justify-center">
+          <div className="bg-white p-6 rounded-lg w-3/4 sm:w-1/2 md:w-1/3 shadow-lg">
+            <h3 className="text-xl font-semibold text-center mb-4">Filter by Category</h3>
+            <div className="flex flex-col space-y-4">
+              <button
+                onClick={() => handleCategoryChange("all")}
+                className={`px-6 py-3 rounded-lg text-lg font-semibold ${selectedCategory === "all" ? "bg-emerald-500 text-white" : "bg-gray-200 text-black"} hover:bg-emerald-400 transition-all duration-200`}
+              >
+                All
+              </button>
+              {categories.map((category) => (
+                <button
+                  key={category.id}
+                  onClick={() => handleCategoryChange(category.id)}
+                  className={`px-6 py-3 rounded-lg text-lg font-semibold ${selectedCategory === category.id ? "bg-emerald-500 text-white" : "bg-gray-200 text-black"} hover:bg-emerald-400 transition-all duration-200`}
+                >
+                  {category.name}
+                </button>
+              ))}
+            </div>
+            <div className="mt-4 text-center">
+              <button
+                onClick={() => setDrawerOpen(false)}
+                className="px-6 py-3 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-all duration-200"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
